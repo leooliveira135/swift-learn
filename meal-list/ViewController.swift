85e7470d92ea892c152de2457e02d1b43e53ec4a
leooliveira135 <oliveiraleonardo61@gmail.com>
2016-07-27 16:21:35 -0300

    Initial Commit

---
+++ meal-list/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meal-list
+//
+//  Created by Leonardo Oliveira on 27/07/16.
+//  Copyright © 2016 Leonardo Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
